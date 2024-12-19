6ef86c381bc4b38982c4e5b9f126665b2fe37625
Caden Buckhalt <halaeb@gmail.com>
2024-12-19 15:09:28 -0600

    prompt fields section

--- src/components/sections/GeospatialPrompts/PromptFields.js
+++ src/components/sections/GeospatialPrompts/PromptFields.js
@@ -4,30 +4,54 @@ import React from 'react';
 import { getFieldId } from '@app/utils/issues';
 import { ValidatedField } from '@components/Form';
 import { Section, Row } from '@components/EditorLayout';
+import NewVariableWindow, { useNewVariableWindowState } from '@components/NewVariableWindow';
 import VariablePicker from '../../Form/Fields/VariablePicker/VariablePicker';
 
-const PromptFields = ({ variable, entity, type }) => (
-  <>
-    <PromptText />
-    <Section title="Selection Variable" id={getFieldId('variable')}>
-      <Row>
-        <ValidatedField
-          name="variable"
-          component={VariablePicker}
-          type={type}
-          entity={entity}
-        //   onCreateOption={handleNewVariable}
-          validation={{ required: true }}
-          variable={variable}
-        />
-      </Row>
-    </Section>
-    <Section title="Layers" id={getFieldId('layers')}>
-      <Row>
-        Configure Map Layers
-      </Row>
-    </Section>
-  </>
-);
+const PromptFields = ({
+  variable, entity, type, changeForm, form,
+}) => {
+  const newVariableWindowInitialProps = {
+    entity,
+    type,
+    initialValues: { name: null, type: 'text' },
+  };
+
+  const handleCreatedNewVariable = (id, { field }) => changeForm(form, field, id);
+
+  const [newVariableWindowProps, openNewVariableWindow] = useNewVariableWindowState(
+    newVariableWindowInitialProps,
+    handleCreatedNewVariable,
+  );
+  const handleNewVariable = (name) => {
+    openNewVariableWindow({ initialValues: { name, type: 'text' } }, { field: 'edgeVariable' });
+  };
+  return (
+    <>
+      <PromptText />
+      <Section title="Selection Variable" id={getFieldId('variable')}>
+        <Row>
+          <ValidatedField
+            name="variable"
+            component={VariablePicker}
+            type={type}
+            entity={entity}
+            onCreateOption={handleNewVariable}
+            validation={{ required: true }}
+            variable={variable}
+          />
+        </Row>
+      </Section>
+      <Section title="Layers" id={getFieldId('layers')}>
+        <Row>
+          Configure Map Layers
+        </Row>
+      </Section>
+      <NewVariableWindow
+              // eslint-disable-next-line react/jsx-props-no-spreading
+        {...newVariableWindowProps}
+      />
+    </>
+  );
+};
 
 export default PromptFields;
